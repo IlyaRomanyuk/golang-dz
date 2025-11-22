aad2872db5c87833f73e9d86178c10e9687d37ec
Илья Романюк <ilya@MacBook-Air-Ila.local>
2025-11-22 18:33:18 +0300

    второе дз

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,5 +7,22 @@ func main() {
     const USD_TO_RUB float64 = 79.1
     const EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
 
-    fmt.Printf("%.2f", EUR_TO_RUB)
+    value, current, target := getUserInput()
+
+    fmt.Println(value, current, target)
+}
+
+func calculateResult(value float64, current string, target string) {
+
+}
+
+func getUserInput() (value float64, current string, target string) {
+	fmt.Print("Введи значение: ")
+	fmt.Scan(&value)
+	fmt.Print("Введи текущую валюту: ")
+	fmt.Scan(&current)
+    fmt.Print("Введи валюту, в которую хотите конвертировать: ")
+	fmt.Scan(&target)
+
+	return
 }
